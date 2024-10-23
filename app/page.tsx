60a5530a019a58fb9810621a2171d8df2876f36a
Your Name <you@example.com>
2024-10-23 11:23:44 +0200

    n.nn.n

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import styles from '../styles/Home.module.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './style.css';
 
 export default function Home() {
     return (
@@ -10,12 +10,12 @@ export default function Home() {
                 <title>Портфолио веб-разработчика Lexa2005</title>
                 <meta name="description" content="Портфолио веб-разработчика" />
             </Head>
-            <div className={styles.backgroundImage}></div>
-            <div className={styles.container}>
+            <div className={'background-image'}></div>
+            <div className={'container'}>
                 <Header />
                 <main>
-                    <section id="about" className={styles.banner}>
-                        <img src="/images/IMG_20241011_110246_391.jpg" alt="Lexa2005" className={styles.profileImage} />
+                    <section id="about" className={'banner'}>
+                        <img src="/images/IMG_20241011_110246_391.jpg" alt="Lexa2005" className={'profileImage'} />
                         <h2>Обо мне</h2>
                         <p>Привет! Я Lexa2005, веб-разработчик с опытом в создании современных и функциональных веб-сайтов. Я люблю решать сложные задачи и создавать удобные интерфейсы.</p>
                     </section>
---
+++ app/projects/page.tsx
@@ -0,0 +1,5 @@
+export default function Projects () {
+    return <div>
+        <h1>hell</h1>
+    </div>
+}
